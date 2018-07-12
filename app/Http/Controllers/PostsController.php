2ec89c22aed105923874c50b8a7b2e57ab918142
Mae <mrcmalubaynz@gmail.com>
2018-07-12 22:36:17 +1200

    Storing images/files

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 
-
+use App\Post;
 use App\Category;
 use Illuminate\Http\Request;
 
@@ -50,6 +50,21 @@ class PostsController extends Controller
         ]);
 
         // dd($request->all());
+
+        $featured = $request->featured;
+
+        $featured_new_name = time().$featured->getClientOriginalName();
+
+        $featured->move('uploads/posts', $featured_new_name);
+
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'featured' => 'uploads/posts/' . $featured_new_name,
+            'category_id' => $request->$category_id
+        ]);
+
+        Session::flash('success','Post Create Successfully!.');
     }
 
     /**

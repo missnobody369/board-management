7b4fffa30e17dd2501ce635ed0d5e02a9b9ed037
Mae <mrcmalubaynz@gmail.com>
2018-08-16 18:37:04 +1200

    finalizing home page for personal portfolio blog

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -15,6 +15,9 @@ class FrontEndController extends Controller
                 ->with('categories', Category::take(5)->get())
                 ->with('first_post', Post::orderBy('created_at', 'desc')->first())
                 ->with('second_post', Post::orderBy('created_at', 'desc')->skip(1)->take(1)->get()->first())
-                ->with('third_post', Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first());
+                ->with('third_post', Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first())
+                ->with('Photoshop', Category::find(3))
+                ->with('InDesign', Category::find(6))
+                ->with('settings', Settings::first());
     }
 }
